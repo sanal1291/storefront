008d39fa165c6d4ed957b64f90a4ad8213ad7f6b
sanalmm <sanalmangadath@gmail.com>
2021-09-19 12:45:02 +0530

    before data models

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x,y = 1,2
+    return x
+
 # Create your views here.
 def say_hello(request):
+    x =calculate()
     return render(request, 'hello.html',{'name':"sanal"})
