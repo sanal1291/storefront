aff0a63919bc0a09f3288abb689ca8fce4789bcc
sanalmm <sanalmangadath@gmail.com>
2021-09-23 00:44:35 +0530

    added many dummy data

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def calculate():
-    x,y = 1,2
-    return x
+from store.models import Product
 
 # Create your views here.
 def say_hello(request):
-    x =calculate()
+    query_set = Product.objects.all()
+    
     return render(request, 'hello.html',{'name':"sanal"})
