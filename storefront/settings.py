529516bb76d469efb4770ee2cd2c5f44004c8418
sanalmm <sanalmangadath@gmail.com>
2021-09-22 19:56:56 +0530

    before django orm

--- storefront/settings.py
+++ storefront/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD':'Sanalsanal@12'
     }
 }
 
